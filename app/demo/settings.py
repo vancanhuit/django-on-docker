710ed796ce72f02f13631fff98108bb5931384f0
vancanhuit <vancanhuit@gmail.com>
2019-07-27 09:34:52 +0700

    Build django in docker

--- app/demo/settings.py
+++ app/demo/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5k%byp#ckf!b#d4ukfd3^i0)e$y51ycy-ub0$-vs^$qou0l_7h'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
